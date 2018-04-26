50ee3ba9e658744ac1988133f5753a4444682b16
Jerry <fenturechance@gmail.com>
2018-04-26 14:19:59 +0800

    add function of create a layer

--- src/components/leafletDraw.vue
+++ src/components/leafletDraw.vue
@@ -10,6 +10,14 @@
             this.$nextTick(()=>{
                 var drawControl = new L.Control.Draw(this.options);
                 this.mapObj.addControl(drawControl);
+                this.mapObj.on(L.Draw.Event.CREATED,(e)=> {
+                    var type = e.layerType,
+                        layer = e.layer;
+                    if (type === 'marker') {
+                        layer.bindPopup('A popup!');
+                    }
+                    this.options.edit.featureGroup.addLayer(layer);
+                });
             })
         },
     }
